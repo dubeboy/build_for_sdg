ca2c56eeae41b0186578176b5b03b1346ec03268
Divine Dube <216004854@student.uj.ac.za>
2020-04-12 02:15:20 +0200

    complete challenge with server implementation

--- src/estimator.py
+++ src/estimator.py
@@ -1,14 +1,17 @@
-# data = reported cases
-# days, weeks, months
-import json
 
 
 def estimator(data):
-    impact = generate_impact(data, 10)
-    generate_severe_cases_by_request_time(impact, data['totalHospitalBeds'])
+    avg_daily_income_population = data['region']['avgDailyIncomePopulation']
+    avg_daily_income_in_usd = data['region']['avgDailyIncomeInUSD']
 
-    severe_impact = generate_impact(data, 50)
-    generate_severe_cases_by_request_time(severe_impact, data['totalHospitalBeds'])
+    impact = generate_impact(data, 10)  # challenge 1
+    generate_severe_cases_by_request_time(impact, data['totalHospitalBeds'])  # challenge 2
+    generate_infections_by_request_time(impact, avg_daily_income_population, avg_daily_income_in_usd)  # challenge 3
+
+    severe_impact = generate_impact(data, 50)  # challenge 1
+    generate_severe_cases_by_request_time(severe_impact, data['totalHospitalBeds'])  # challenge 2
+    generate_infections_by_request_time(severe_impact, avg_daily_income_population,
+                                        avg_daily_income_in_usd)  # challenge 3
 
     data = {'data': data, 'impact': impact, 'severeImpact': severe_impact}
     return data
@@ -27,38 +30,15 @@ def generate_impact(data, multiplier):
         days = infections_by_requested_time
 
     return {'currentlyInfected': currentlyInfected,
-            'infectionsByRequestedTime': currentlyInfected * (2 ** int(days / 3))}
-
-
-def main():
-    js = """
-      {
-    "region": {
-      "name": "Africa",
-      "avgAge": 19.7,
-      "avgDailyIncomeInUSD": 5,
-      "avgDailyIncomePopulation": 0.71
-    },
-    "periodType": "days",
-    "timeToElapse": 28,
-    "reportedCases": 50,
-    "population": 66622705,
-    "totalHospitalBeds": 1380614
-  }
-    """
-    js = json.loads(js)
-    print(estimator(js))
-
-
-main()
+            'infectionsByRequestedTime': currentlyInfected * int(2 ** (days / 3))}
 
 
 # Challenge two
 def generate_severe_cases_by_request_time(impact_data, total_hospital_beds):
     infections_by_requested_time = impact_data['infectionsByRequestedTime']
 
-    bed_availability = total_hospital_beds * 0.35
-    severe_cases_by_request_time = infections_by_requested_time * 0.15
+    bed_availability = int(total_hospital_beds * 0.35)
+    severe_cases_by_request_time = int(infections_by_requested_time * 0.15)
 
     impact_data['severeCasesByRequestedTime'] = severe_cases_by_request_time
     impact_data['hospitalBedsByRequestedTime'] = severe_cases_by_request_time - bed_availability
@@ -73,7 +53,7 @@ def generate_infections_by_request_time(impact_data, avg_daily_income_population
 
     dollars_in_flight = infections_by_requested_time * avg_daily_income_population * avg_daily_income_in_usd * 30
 
-    impact_data['casesForICUByRequestedTime'] = cases_for_ICU_by_requested_time
-    impact_data['casesForVentilatorsByRequestedTime'] = cases_for_ventilators_by_requested_time
+    impact_data['casesForICUByRequestedTime'] = int(cases_for_ICU_by_requested_time)
+    impact_data['casesForVentilatorsByRequestedTime'] = int(cases_for_ventilators_by_requested_time)
     impact_data['dollarsInFlight'] = round(dollars_in_flight, 2)
 
