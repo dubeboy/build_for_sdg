cdd5977a49d9f42ccce097604fa83028def5a9ab
Divine Dube <216004854@student.uj.ac.za>
2020-04-12 06:41:15 +0200

    Fix small bugs

--- src/estimator.py
+++ src/estimator.py
@@ -1,33 +1,30 @@
+import json
 
 
 def estimator(data):
     avg_daily_income_population = data['region']['avgDailyIncomePopulation']
     avg_daily_income_in_usd = data['region']['avgDailyIncomeInUSD']
+    time_to_elapse = data['timeToElapse']
+    period_type = data['periodType']
 
-    impact = generate_impact(data, 10)  # challenge 1
+    impact = generate_impact(data, 10, period_type, time_to_elapse)  # challenge 1
     generate_severe_cases_by_request_time(impact, data['totalHospitalBeds'])  # challenge 2
-    generate_infections_by_request_time(impact, avg_daily_income_population, avg_daily_income_in_usd)  # challenge 3
+    generate_infections_by_request_time(impact, avg_daily_income_population,
+                                        avg_daily_income_in_usd,period_type, time_to_elapse)  # challenge 3
 
-    severe_impact = generate_impact(data, 50)  # challenge 1
+    severe_impact = generate_impact(data, 50, period_type, time_to_elapse)  # challenge 1
     generate_severe_cases_by_request_time(severe_impact, data['totalHospitalBeds'])  # challenge 2
     generate_infections_by_request_time(severe_impact, avg_daily_income_population,
-                                        avg_daily_income_in_usd)  # challenge 3
+                                        avg_daily_income_in_usd, period_type, time_to_elapse)  # challenge 3
 
     data = {'data': data, 'impact': impact, 'severeImpact': severe_impact}
     return data
 
 
-def generate_impact(data, multiplier):
+# Challenge one
+def generate_impact(data, multiplier, period_type, time_to_elapse):
     currentlyInfected = data["reportedCases"] * multiplier
-    infections_by_requested_time = data['timeToElapse']
-    period_type = data['periodType']
-
-    if period_type == 'weeks':
-        days = infections_by_requested_time * 7
-    elif period_type == 'months':
-        days = infections_by_requested_time * 30
-    else:
-        days = infections_by_requested_time
+    days = get_days(period_type, time_to_elapse)
 
     return {'currentlyInfected': currentlyInfected,
             'infectionsByRequestedTime': currentlyInfected * int(2 ** (days / 3))}
@@ -35,25 +32,39 @@ def generate_impact(data, multiplier):
 
 # Challenge two
 def generate_severe_cases_by_request_time(impact_data, total_hospital_beds):
-    infections_by_requested_time = impact_data['infectionsByRequestedTime']
-
+    severe_cases_by_request_time = int(impact_data['infectionsByRequestedTime'] * 0.15)
     bed_availability = int(total_hospital_beds * 0.35)
-    severe_cases_by_request_time = int(infections_by_requested_time * 0.15)
 
     impact_data['severeCasesByRequestedTime'] = severe_cases_by_request_time
-    impact_data['hospitalBedsByRequestedTime'] = severe_cases_by_request_time - bed_availability
+    impact_data['hospitalBedsByRequestedTime'] = bed_availability - severe_cases_by_request_time
 
 
 # Challenge 3
-def generate_infections_by_request_time(impact_data, avg_daily_income_population, avg_daily_income_in_usd):
+def generate_infections_by_request_time(impact_data,
+                                        avg_daily_income_population,
+                                        avg_daily_income_in_usd,
+                                        period_type,
+                                        time_to_elapse):
     infections_by_requested_time = impact_data['infectionsByRequestedTime']
 
     cases_for_ICU_by_requested_time = infections_by_requested_time * 0.05
     cases_for_ventilators_by_requested_time = infections_by_requested_time * 0.02
 
-    dollars_in_flight = infections_by_requested_time * avg_daily_income_population * avg_daily_income_in_usd * 30
+    days = get_days(period_type, time_to_elapse)
+    dollars_in_flight = infections_by_requested_time * avg_daily_income_population * avg_daily_income_in_usd * days
 
     impact_data['casesForICUByRequestedTime'] = int(cases_for_ICU_by_requested_time)
     impact_data['casesForVentilatorsByRequestedTime'] = int(cases_for_ventilators_by_requested_time)
     impact_data['dollarsInFlight'] = round(dollars_in_flight, 2)
 
+
+# Private methods
+def get_days(period_type, time_to_elapse):
+    if period_type == 'weeks':
+        days = time_to_elapse * 7
+    elif period_type == 'months':
+        days = time_to_elapse * 30
+    else:
+        days = time_to_elapse
+
+    return days
